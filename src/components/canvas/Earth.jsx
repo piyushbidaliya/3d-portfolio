62806bbf702eb4ffd76ec1589b5a7635881a0847
Piyush Bidaliya <piyushbidaliya@gmail.com>
2025-05-24 15:25:34 +0530

    gltf to glb

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
 const Earth = () => {
-  const earth = useGLTF('./planet/scene.gltf')
+  const earth = useGLTF('./planet/scene.glb')
   return (
     <primitive
       object={earth.scene}
